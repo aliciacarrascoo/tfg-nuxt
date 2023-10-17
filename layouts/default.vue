b39d2a811f9952c4058f255efa1a60dd37efcf0b
Alicia <alex.dickson@immfly.com>
2023-10-17 22:56:30 +0200

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <NuxtLink class="navbar-brand px-3" to="/">MalwareScan</NuxtLink>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <NuxtLink class="nav-link" to="/logs">Análisis logs (Cortex XDR)</NuxtLink>
+                    <NuxtLink class="nav-link" to="/headers">Análisis cabeceras mail</NuxtLink>
+                    <NuxtLink class="nav-link" to="/resources">Recursos</NuxtLink>
+                    <NuxtLink class="nav-link" to="/databases">Bases de datos de malware</NuxtLink>
+                    <NuxtLink class="nav-link" to="/blog">Blog</NuxtLink>
+                </div>
+                </div>
+            </nav>
+        </header>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
---
+++ pages/logs.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-4 vh-100">
+        <h1 class="title mb-5">Logs</h1>
+        <div class="form-group h-50 ">
+            <label for="exampleFormControlTextarea1">Introduce logs</label>
+            <textarea class="form-control h-25" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <div class="w-100 d-fle">
+                <div class="mr-2">Example file input</div>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+            <button class="btn btn-primary mt-3 ml-auto">Analizar</button>
+        </div>
+    </div>
+</template>
