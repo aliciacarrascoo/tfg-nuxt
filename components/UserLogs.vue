21317c082dce1ce62696c1b966660c082c1a46cd
Alicia <alex.dickson@immfly.com>
2023-10-23 00:12:30 +0200

    backend works

--- components/UserLogs.vue
+++ components/UserLogs.vue
@@ -3,12 +3,10 @@ const client = useSupabaseClient()
     const user = useSupabaseUser()
   const logs = ref([])
   const {data} = await useAsyncData('logs', async () => {
-    return await client.from('logs').select('id, log').eq('user', user.value.id).order('created_at')
-  },{
-    transform: result => result.body
+    return await client.from('logs').select('id, content')
   })
-  console.log(data)
+  console.log("backend:", data)
 </script>
 <template>
-
+    {{ data }}
 </template>
